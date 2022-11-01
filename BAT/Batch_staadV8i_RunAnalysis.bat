3f691c2188d3c0bd452ee5ef1f00cf99f60ad09a
Vivek Patel <vivek@nodesautomations.com>
2022-11-02 00:12:25 +0530

    Create Batch_staadV8i_RunAnalysis.bat

---
+++ BAT/Batch_staadV8i_RunAnalysis.bat
@@ -0,0 +1,3 @@
+"C:\SProV8i SS6\STAAD\SProStaad\SProStaad.exe" /s STAAD "C:\Users\vkpun\Desktop\BoxCulvertAddin\BoxCulvert_Std3D\Quasi.std"
+"C:\SProV8i SS6\STAAD\SProStaad\SProStaad.exe" /s STAAD "C:\Users\vkpun\Desktop\BoxCulvertAddin\BoxCulvert_Std3D\Deflection.std"
+"C:\SProV8i SS6\STAAD\SProStaad\SProStaad.exe" /s STAAD "C:\Users\vkpun\Desktop\BoxCulvertAddin\BoxCulvert_Std3D\Quasi.std"
